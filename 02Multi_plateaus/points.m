27867352c57270c753ee27fa35a95c93ae85b3b8
YXTANGXMU <68266998+YXTANGXMU@users.noreply.github.com>
2020-07-14 18:03:09 +0800

    Update points.m

--- 02Multi_plateaus/points.m
+++ 02Multi_plateaus/points.m
@@ -1,7 +1,7 @@
  function  p=points(dis,data)
 for i=1:length(data)
-if abs(abs(data(i,1)-data(2,1))-dis)<0.0001
-        p=i-2;
+if abs(abs(data(i,1)-data(5,1))-dis)<0.0001
+        p=i-4;
 break
 end
 end
